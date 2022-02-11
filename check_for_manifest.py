feee236eb11fa39703554a2e085b2bca2ec047ad
Tian-que <1605206150@qq.com>
2022-02-11 19:32:27 +0800

    test

---
+++ check_for_manifest.py
@@ -0,0 +1,37 @@
+import os
+import asyncio
+
+import httpx
+
+
+async def get_content(url, headers):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url=url, headers=headers)
+        return response.json()
+
+
+async def getManifestOnline() -> dict:
+    """
+    :说明: `getManifestOnline`
+    > 在线获取manifest信息
+
+    :返回:
+        - `dict`: Bungieapi返回值
+    """
+    url = "https://www.bungie.net/Platform/Destiny2/Manifest/"
+    headers = {"X-API-KEY": "{}".format(os.environ.get("API_KEY"))}
+    data = await get_content(url, headers)
+    if data.get("ErrorCode") == 1 and data.get("Message") == "Ok":
+        return data["Response"]
+    else:
+        raise
+
+
+async def main():
+    # manifest = await getManifestOnline()
+    # print(manifest)
+    print(os.environ.get("LOG_LEVEL"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
