0e94ddbe91907e38377f16f827847058c2288fe5
Tian-que <1605206150@qq.com>
2022-02-11 20:37:47 +0800

    test

--- check_for_manifest.py
+++ check_for_manifest.py
@@ -4,6 +4,16 @@ import asyncio
 import httpx
 
 
+def writeFile(path: str, content: str):
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(content)
+
+
+def readFile(path: str) -> str:
+    with open(path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
 async def get_content(url, headers):
     async with httpx.AsyncClient() as client:
         response = await client.get(url=url, headers=headers)
@@ -11,13 +21,6 @@ async def get_content(url, headers):
 
 
 async def getManifestOnline() -> dict:
-    """
-    :说明: `getManifestOnline`
-    > 在线获取manifest信息
-
-    :返回:
-        - `dict`: Bungieapi返回值
-    """
     url = "https://www.bungie.net/Platform/Destiny2/Manifest/"
     headers = {"X-API-KEY": "{}".format(os.environ.get("API_KEY"))}
     data = await get_content(url, headers)
@@ -28,8 +31,17 @@ async def getManifestOnline() -> dict:
 
 
 async def main():
-    manifest = await getManifestOnline()
-    print("Manifest version: ", manifest["version"])
+    latest = readFile("./latest.json")
+    manifestMetadata = await getManifestOnline()
+    current = manifestMetadata["version"]
+    newREADME = "# py-d2-manifest-bot\ngithub action for checking for new d2 manifest\n\n# Current Manifest: {current}"
+    print(f"Latest: {latest}")
+    print(f"Current: {current}")
+    if latest == current:
+        return
+    print("New manifest detected")
+    writeFile("latest.json", current)
+    writeFile("README.md", newREADME.format(current=current))
 
 
 if __name__ == "__main__":
